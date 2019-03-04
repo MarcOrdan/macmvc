ffe3b9018d436bb03a4f8a8b183322aba7b6f86c
Mac Ordan <marcordan@gmail.com>
2019-03-04 11:00:43 +0800

    build a header and footer includes

--- app/controllers/Pages.php
+++ app/controllers/Pages.php
@@ -13,7 +13,11 @@
         }
 
         public function about() {
-            $this->view('pages/about');
+            $data = [
+                'title' => 'About Us'
+            ];
+
+            $this->view('pages/about', $data);
         }
 
     }
