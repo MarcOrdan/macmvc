a36fc0a90c81534e8d8646a11ee40f0f6b60fa47
Mac Ordan <marcordan@gmail.com>
2019-03-04 13:26:54 +0800

    Clean and finalize the mvc framework

--- app/config/config.php
+++ app/config/config.php
@@ -1,15 +1,18 @@
 <?php 
     //DB PARAMS
     define('DB_HOST','localhost');
-    define('DB_USER', 'root');
-    define('DB_PASS','123456');
-    define('DB_NAME','tmvc');
+    define('DB_USER', 'YOUR_USER');
+    define('DB_PASS','YOUR_PASS');
+    define('DB_NAME','YOUR_DBNAME');
 
     //APP ROOT
     define('APPROOT', dirname(dirname(__FILE__)));
 
     //URL ROOT
+    // define('URLROOT','YOUR_URL');
     define('URLROOT','https://localhost/macmvc');
 
+
     //SITE NAME
+    // define('SITENAME','YOUR_SITENAME');
     define('SITENAME','MacMVC');
