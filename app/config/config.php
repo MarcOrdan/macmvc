0b9cb2d9cf8cf9ffdc28c9b4e0d3f589c4ec9992
Mac Ordan <marcordan@gmail.com>
2019-03-04 10:33:04 +0800

    Build code for config file and autoload spl

---
+++ app/config/config.php
@@ -0,0 +1,9 @@
+<?php 
+    //APP ROOT
+    define('APPROOT', dirname(dirname(__FILE__)));
+
+    //URL ROOT
+    define('URLROOT','https://localhost/macmvc');
+
+    //SITE NAME
+    define('SITENAME','MacMVC');
