e4663e5ea070bf6d8d7641df1f2c75f8d981d070
Mac Ordan <marcordan@gmail.com>
2019-03-04 13:17:36 +0800

    finalize database class

--- app/libraries/database.php
+++ app/libraries/database.php
@@ -1,4 +1,5 @@
 <?php
+
     /* PDO DATABASE CLASS
     *CONNECT TO DB
     *CREATE PREPARED STATEMENTS
@@ -14,7 +15,8 @@
         private $dbh;
         private $stmt;
         private $error;
-
+        
+        //DOES THE CONNECTION
         public function __construct(){
             //set dsn
             $dsn = 'mysql:host=' . $this->host . ';dbname=' . $this->dbname;
@@ -32,5 +34,54 @@
                 echo $this->error;
             }
         }
+
+        //prepare statement query
+        public function query($sql){
+            $this->stmt = $this->dbh->prepare($sql);
+        }
+
+        //bind values
+        public function bind($param, $value, $type = null){
+            if(is_null($type)){
+                switch(true){
+                    case is_int($value):
+                        $type = PDO::PARAM_INT;
+                        break;
+                    case is_bool($value):
+                        $type = PDO::PARAM_BOOL;
+                        break;  
+                    case is_null($value):
+                        $type = PDO::PARAM_NULL;
+                        break; 
+                    default:
+                        $type = PDO::PARAM_STR;
+                        break;
+                }
+            }
+
+            $this->stmt->bindValue($param, $value, $type);
+        }
+
+        //execute the prepared statement
+        public function execute(){
+            return $this->stmt->execute();
+        }
         
+        //get result set(many result) as array of objects
+        public function resultSet(){
+            $this->execute();
+            return $this->stmt->fetchAll(PDO::FETCH_OBJ);
+        }
+
+         //get single row result as object
+        public function single(){
+            $this->execute();
+             return $this->stmt->fetch(PDO::FETCH_OBJ);
+         }
+
+         public function rowCount(){
+             return $this->stmt->rowCount();
+         }
+
+
     }
