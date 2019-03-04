8c9d0b659dee5d991e88a091061a8a2c6351ae82
Mac Ordan <marcordan@gmail.com>
2019-03-04 12:33:49 +0800

    create a database class part 1

--- app/config/config.php
+++ app/config/config.php
@@ -1,4 +1,10 @@
 <?php 
+    //DB PARAMS
+    define('DB_HOST','localhost');
+    define('DB_USER', 'root');
+    define('DB_PASS','123456');
+    define('DB_NAME','tmvc');
+
     //APP ROOT
     define('APPROOT', dirname(dirname(__FILE__)));
 
---
+++ app/libraries/database.php
@@ -0,0 +1,36 @@
+<?php
+    /* PDO DATABASE CLASS
+    *CONNECT TO DB
+    *CREATE PREPARED STATEMENTS
+    *BIND VALUES
+    *RETURN ROWS AND RESULTS
+    */
+    class Database {
+        private $host = DB_HOST;
+        private $user = DB_USER;
+        private $pass = DB_PASS;
+        private $dbname = DB_NAME;
+
+        private $dbh;
+        private $stmt;
+        private $error;
+
+        public function __construct(){
+            //set dsn
+            $dsn = 'mysql:host=' . $this->host . ';dbname=' . $this->dbname;
+            $options = array(
+                PDO::ATTR_PERSISTENT => true,
+                PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
+            );
+
+            //create pdo instance
+            try{
+                $this->dbh = new PDO($dsn,$this->user,$this->pass,$options);
+
+            } catch(PDOException $e){
+                $this->error = $e->getMessage();
+                echo $this->error;
+            }
+        }
+        
+    }
